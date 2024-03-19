fbe582859cafc0e6211099be5973d04a5040fc93
ValeryPotrebina <valery_potrebina_2003@mail.ru>
2024-03-19 15:34:41 +0300

    added slider

--- js/app.js
+++ js/app.js
@@ -81,3 +81,35 @@ $(".modal__dialog").on("click", function (event) {
 });
 
 //Изучить Jquare
+
+/*
+ * Slider: https://kenwheeler.github.io/slick/
+ */
+
+$('[data-slider="slick"]').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true
+});
+
+
+$(".slickPrev").on("click", function (event) {
+    event.preventDefault();
+
+    let currentSlider =
+        $(this).parents('.modal').find('[data-slider="slick"]');
+
+    currentSlider.slick("slickPrev");
+})
+
+$(".slickNext").on("click", function (event) {
+    event.preventDefault();
+
+    let currentSlider =
+        $(this).parents('.modal').find('[data-slider="slick"]');
+
+        currentSlider.slick("slickNext");
+})
